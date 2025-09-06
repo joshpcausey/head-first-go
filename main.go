0712ea7cd5f97895b03e292f14bbca264a395af2
Joshua Causey <joshpcausey@gmail.com>
2025-09-06 18:09:52 -0500

    moving stuff around

--- main.go
+++ main.go
@@ -1,14 +1,4 @@
 package main
-
-import (
-	"fmt"
-	"head-first-go/hello"
-
-	"github.com/enescakir/emoji"
-)
-
 func main() {
-	hello.Hello()
-
-	fmt.Println(emoji.WavingHand.Tone())
+	read-text-file.
 }
--- read-text-file/go.mod
+++ read-text-file/go.mod
@@ -1,3 +1,3 @@
-module readfile
+module readTextFile
 
 go 1.25.1
